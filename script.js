1dcbd58fecaaf6fb53317d50cd70c963e6a6301c
mentorminero <73854241+mentorminero@users.noreply.github.com>
2023-08-26 16:42:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// script.js
 document.addEventListener("DOMContentLoaded", function () {
-    // Acceder a la variable de entorno API_KEY
-    const apiKey = process.env.API_KEY;
+    // Acceder a la variable de entorno API_KEY (en este caso, usando una variable global)
+    const apiKey = window.API_KEY;
 
     // Mostrar la API_KEY en el párrafo con id "api-key"
     document.getElementById("api-key").textContent = apiKey;
